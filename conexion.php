e9710103dd7261a1c52d795d2a55d4c1eee065f2
Asnamel <81118376+angelitooooo3131@users.noreply.github.com>
2021-04-08 20:20:13 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,4 @@
 <?php
-
 $connectionInfo = array("UID" => "pipo", "pwd" => "{your_password_here}", "Database" => "clinete2", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:ztaweb.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
